eabb106704658710d279805b715e5b4a9de8492c
anol123 <anol.roy123@gmail.com>
2026-01-10 17:56:38 +0530

    sum of elements in array

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     let arr = [1,2,3,4,5];
+    let mut sum =0;
 
     for i in 0..arr.len(){
-        println!("{}",arr[i]);
+        sum+=arr[i];
     }
+    println!("Sum of all elements in array is: {}", sum);
+    
 }
