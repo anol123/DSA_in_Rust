9f2740cccc46b4746445e3c73e8c6e1668a36f79
anol123 <anol.roy123@gmail.com>
2026-01-13 23:01:00 +0530

    working

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -1,12 +1,28 @@
 mod array_1;
 fn main() {
    let mut v = vec![1,2,3,4,5];
-   v.push(2);
-   println!("{:?}",v);  
+   // v.push(2);
+   // println!("{:?}",v);  
 
-   for i in 0..v.len(){
-        println!("{}",v[i]);
-        //v.push(2*i); //since v.len() value is already decided at the start of loop, so the loop will run 6 times and print the first 5 elements only
+   // for i in 0..v.len(){
+   //      println!("{}",v[i]);
+   //      //v.push(2*i); //since v.len() value is already decided at the start of loop, so the loop will run 6 times and print the first 5 elements only
 
+   // }
+
+   let mut i = 0;
+   let mut j = v.len()-1;
+
+   while i<j{
+      v.swap(i, j);
+      // v[i]=v[i]+v[j];
+      // v[j]=v[i]-v[j];
+      // v[i]=v[i]- v[j];
+      i+=1;
+      j-=1;
    }
+
+   println!("{:?}",v);
+
+
 }
