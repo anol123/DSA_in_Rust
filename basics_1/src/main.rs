c93bf45d3a2c25b3d42da7d2056f3224f3b56d2c
anol123 <anol.roy123@gmail.com>
2026-01-13 22:49:38 +0530

    wip

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -1,9 +1,12 @@
 mod array_1;
 fn main() {
-   let v = vec![1,2,3,4,5];
+   let mut v = vec![1,2,3,4,5];
+   v.push(2);
    println!("{:?}",v);  
 
    for i in 0..v.len(){
         println!("{}",v[i]);
+        //v.push(2*i); //since v.len() value is already decided at the start of loop, so the loop will run 6 times and print the first 5 elements only
+
    }
 }
