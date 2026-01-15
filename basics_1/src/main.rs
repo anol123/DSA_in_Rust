81e393ac48257898ad4f0a4932f10415f823fac5
anol123 <anol.roy123@gmail.com>
2026-01-15 23:08:35 +0530

    wrote swap fn in rust

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -1,28 +1,30 @@
 mod array_1;
 fn main() {
-   let mut v = vec![1,2,3,4,5];
-   // v.push(2);
-   // println!("{:?}",v);  
+    let mut v = vec![1, 2, 3, 4, 5];
+    // v.push(2);
+    // println!("{:?}",v);
 
-   // for i in 0..v.len(){
-   //      println!("{}",v[i]);
-   //      //v.push(2*i); //since v.len() value is already decided at the start of loop, so the loop will run 6 times and print the first 5 elements only
+    // for i in 0..v.len(){
+    //      println!("{}",v[i]);
+    //      //v.push(2*i); //since v.len() value is already decided at the start of loop, so the loop will run 6 times and print the first 5 elements only
 
-   // }
+    // }
 
-   let mut i = 0;
-   let mut j = v.len()-1;
+    let mut i = 0;
+    let mut j = v.len() - 1;
 
-   while i<j{
-      //v.swap(i, j);
-      v[i]=v[i]+v[j];
-      v[j]=v[i]-v[j];
-      v[i]=v[i]- v[j];
-      i+=1;
-      j-=1;
-   }
+    while i < j {
+        swap(&mut v, i, j);
 
-   println!("{:?}",v);
+        i += 1;
+        j -= 1;
+    }
 
+    println!("{:?}", v);
+}
 
+fn swap(v: &mut Vec<i32>, i: usize, j: usize) {
+    v[i] = v[i] + v[j];
+    v[j] = v[i] - v[j];
+    v[i] = v[i] - v[j];
 }
