307db60c456ea69fbd63dae143a66e8cda6b2b8c
anol123 <anol.roy123@gmail.com>
2026-01-29 21:03:01 +0530

    working

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -8,8 +8,12 @@ fn main() {
     let mut v = vec![2, 5, 3, 9, 5, 6, 1];
 
     bubble_sort(&mut v);
+    selection_sort(&mut v);
 }
 
 pub fn bubble_sort(v: &mut Vec<i32>) {
+    todo!()
+}
+pub fn selection_sort(v: &mut Vec<i32>) {
     todo!()
 }
