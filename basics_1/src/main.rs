aa8d1994f7966ceea86454f9351066510a0e215a
anol123 <anol.roy123@gmail.com>
2026-02-03 22:18:10 +0530

    bubble sort added

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -1,3 +1,5 @@
+use std::mem::swap;
+
 mod array_1;
 mod search;
 mod vec_1;
@@ -8,13 +10,25 @@ fn main() {
     let mut v = vec![2, 5, 3, 9, 5, 6, 1];
 
     bubble_sort(&mut v);
-    selection_sort(&mut v);
-    insertion_sort(&mut v);
-    merge_sort(&mut v);
+    println!("{:?}", v);
+    // selection_sort(&mut v);
+    // insertion_sort(&mut v);
+    // merge_sort(&mut v);
 }
 
 pub fn bubble_sort(v: &mut Vec<i32>) {
-    todo!()
+    for i in 0..v.len() {
+        let mut swapped = false;
+        for j in 0..v.len() - i - 1 {
+            if (v[i] > v[j + 1]) {
+                v.swap(i, j + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
 }
 pub fn selection_sort(v: &mut Vec<i32>) {
     todo!()
