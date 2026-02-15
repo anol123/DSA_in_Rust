46d13a0f9258053a1a39d95f753a905b0fc8f244
anol123 <anol.roy123@gmail.com>
2026-02-15 23:44:09 +0530

    working with insertion sort

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -10,8 +10,8 @@ fn main() {
     let mut v = vec![2, 5, 3, 9, 5, 6, 1];
 
     bubble_sort(&mut v);
-    selection_sort(&mut v);
-    //insertion_sort(&mut v);
+    //selection_sort(&mut v);
+    insertion_sort(&mut v);
     // merge_sort(&mut v);
     println!("{:?}", v);
 }
@@ -31,22 +31,22 @@ pub fn bubble_sort(v: &mut Vec<i32>) {
         }
     }
 }
-pub fn selection_sort(v: &mut Vec<i32>) {
-    println!("!------------------Sorting through selection sort------------------!");
-    let n = v.len();
+// pub fn selection_sort(v: &mut Vec<i32>) {
+//     println!("!------------------Sorting through selection sort------------------!");
+//     let n = v.len();
 
-    for i in 0..n {
-        let mut min_idx = i;
+//     for i in 0..n {
+//         let mut min_idx = i;
 
-        for j in i + 1..n {
-            if v[j] < v[min_idx] {
-                min_idx = j;
-            }
-        }
+//         for j in i + 1..n {
+//             if v[j] < v[min_idx] {
+//                 min_idx = j;
+//             }
+//         }
 
-        v.swap(i, min_idx);
-    }
-}
+//         v.swap(i, min_idx);
+//     }
+// }
 
 pub fn insertion_sort(v: &mut Vec<i32>) {
     println!("!------------------Sorting through insertion sort------------------!");
