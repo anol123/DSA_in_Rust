cd54be707b25b729370bb5b0517a44039ffb1357
anol123 <anol.roy123@gmail.com>
2026-02-15 23:45:20 +0530

    working with selection sort

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -10,8 +10,8 @@ fn main() {
     let mut v = vec![2, 5, 3, 9, 5, 6, 1];
 
     bubble_sort(&mut v);
-    //selection_sort(&mut v);
-    insertion_sort(&mut v);
+    selection_sort(&mut v);
+    //insertion_sort(&mut v);
     // merge_sort(&mut v);
     println!("{:?}", v);
 }
@@ -31,40 +31,40 @@ pub fn bubble_sort(v: &mut Vec<i32>) {
         }
     }
 }
-// pub fn selection_sort(v: &mut Vec<i32>) {
-//     println!("!------------------Sorting through selection sort------------------!");
-//     let n = v.len();
-
-//     for i in 0..n {
-//         let mut min_idx = i;
-
-//         for j in i + 1..n {
-//             if v[j] < v[min_idx] {
-//                 min_idx = j;
-//             }
-//         }
-
-//         v.swap(i, min_idx);
-//     }
-// }
-
-pub fn insertion_sort(v: &mut Vec<i32>) {
-    println!("!------------------Sorting through insertion sort------------------!");
-
+pub fn selection_sort(v: &mut Vec<i32>) {
+    println!("!------------------Sorting through selection sort------------------!");
     let n = v.len();
 
-    for i in 1..n {
-        let key = v[i];
-        let mut j = i;
+    for i in 0..n {
+        let mut min_idx = i;
 
-        while j > 0 && v[j - 1] > key {
-            v[j] = v[j - 1];
-            j -= 1;
+        for j in i + 1..n {
+            if v[j] < v[min_idx] {
+                min_idx = j;
+            }
         }
 
-        v[j] = key;
+        v.swap(i, min_idx);
     }
 }
+
+// pub fn insertion_sort(v: &mut Vec<i32>) {
+//     println!("!------------------Sorting through insertion sort------------------!");
+
+//     let n = v.len();
+
+//     for i in 1..n {
+//         let key = v[i];
+//         let mut j = i;
+
+//         while j > 0 && v[j - 1] > key {
+//             v[j] = v[j - 1];
+//             j -= 1;
+//         }
+
+//         v[j] = key;
+//     }
+// }
 pub fn merge_sort(v: &mut Vec<i32>) {
     todo!()
 }
