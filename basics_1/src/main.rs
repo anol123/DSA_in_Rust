58c49eb029784c88938b7feba907e83a536a3e43
anol123 <anol.roy123@gmail.com>
2026-03-12 21:05:17 +0530

    changed the code

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -9,28 +9,28 @@ fn main() {
 
     let mut v = vec![2, 5, 3, 9, 5, 6, 1];
 
-    //bubble_sort(&mut v);
+    bubble_sort(&mut v);
     //selection_sort(&mut v);
     //insertion_sort(&mut v);
-    merge_sort(&mut v);
+    //merge_sort(&mut v);
     println!("{:?}", v);
 }
 
-// pub fn bubble_sort(v: &mut Vec<i32>) {
-//     println!("!------------------Sorting through bubble sort------------------!");
-//     for i in 0..v.len() {
-//         let mut swapped = false;
-//         for j in 0..v.len() - i - 1 {
-//             if (v[j] > v[j + 1]) {
-//                 v.swap(j, j + 1);
-//                 swapped = true;
-//             }
-//         }
-//         if !swapped {
-//             break;
-//         }
-//     }
-// }
+pub fn bubble_sort(v: &mut Vec<i32>) {
+    println!("!------------------Sorting through bubble sort------------------!");
+    for i in 0..v.len() {
+        let mut swapped = false;
+        for j in 0..v.len() - i - 1 {
+            if (v[j] > v[j + 1]) {
+                v.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
+}
 // pub fn selection_sort(v: &mut Vec<i32>) {
 //     println!("!------------------Sorting through selection sort------------------!");
 //     let n = v.len();
@@ -48,23 +48,23 @@ fn main() {
 //     }
 // }
 
-pub fn insertion_sort(v: &mut Vec<i32>) {
-    println!("!------------------Sorting through insertion sort------------------!");
+// pub fn insertion_sort(v: &mut Vec<i32>) {
+//     println!("!------------------Sorting through insertion sort------------------!");
 
-    let n = v.len();
+//     let n = v.len();
 
-    for i in 1..n {
-        let key = v[i];
-        let mut j = i;
+//     for i in 1..n {
+//         let key = v[i];
+//         let mut j = i;
 
-        while j > 0 && v[j - 1] > key {
-            v[j] = v[j - 1];
-            j -= 1;
-        }
+//         while j > 0 && v[j - 1] > key {
+//             v[j] = v[j - 1];
+//             j -= 1;
+//         }
 
-        v[j] = key;
-    }
-}
-pub fn merge_sort(v: &mut Vec<i32>) {
-    todo!()
-}
+//         v[j] = key;
+//     }
+// }
+// pub fn merge_sort(v: &mut Vec<i32>) {
+//     todo!()
+// }
